ef769c53d16c3d3c9858ded1c9fd3424b94008ef
Felipe Santos <felipecassiors@gmail.com>
2020-05-27 21:08:39 -0300

    Download dependencies on the fly

--- Tools/SandboxTest.ps1
+++ Tools/SandboxTest.ps1
@@ -2,7 +2,7 @@
 
 param ([string] $Manifest = $(throw "The Manifest parameter is required."))
 
-if (-not (Test-Path -Path $Manifest)) {
+if (-not (Test-Path -Path $Manifest -PathType Leaf)) {
   throw 'The Manifest file does not exist.'
 }
 
@@ -13,19 +13,57 @@ if ($validationResult -like '*Manifest validation failed.*') {
   throw 'Manifest validation failed.'
 }
 
+# Set dependencies
+
+$desktopAppInstaller = @{
+  fileName = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
+  url      = 'https://github.com/microsoft/winget-cli/releases/download/v0.1.4331-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
+  hash     = 'e1b5aa89c7354dd39db38c2ac33f0455c4529eb4cf77f023028d955122ea8377'
+}
+
+$vcLibs = @{
+  fileName = 'Microsoft.VCLibs.140.00_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
+  url      = 'https://github.com/felipecassiors/winget-pkgs/raw/da8548d90369eb8f69a4738dc1474caaffb58e12/Tools/SandboxTest_Temp/Microsoft.VCLibs.140.00_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
+  hash     = 'fe660c46a3ff8462d9574902e735687e92eeb835f75ec462a41ef76b54ef13ed'
+}
+
+$vcLibsUwp = @{
+  fileName = 'Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
+  url      = 'https://raw.githubusercontent.com/felipecassiors/winget-pkgs/da8548d90369eb8f69a4738dc1474caaffb58e12/Tools/SandboxTest_Temp/Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
+  hash     = '66de9fde9d2ebf18893a890987f35d2d145c18cc5ee0e8ecaa09477dcc13b16b'
+}
+
+$dependencies = @($desktopAppInstaller, $vcLibs, $vcLibsUwp)
+
 # Initialize Temp Folder
 
 $tempFolder = Join-Path -Path $PSScriptRoot -ChildPath 'SandboxTest_Temp'
 
-Get-ChildItem $tempFolder -Recurse -Exclude *.appx, *.appxbundle | Remove-Item -Force
+Get-ChildItem $tempFolder -Recurse -Exclude $dependencies.fileName | Remove-Item -Force
 
 Copy-Item -Path $Manifest -Destination $tempFolder
 
-# TODO: Download dependencies
+# Download dependencies
 
-$desktopAppInstaller = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle'
-$vcLibs = 'Microsoft.VCLibs.140.00_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
-$vcLibsUwp = 'Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x64__8wekyb3d8bbwe.Appx'
+$WebClient = New-Object System.Net.WebClient
+
+foreach ($dependency in $dependencies) {
+  $dependency.file = Join-Path -Path $tempFolder -ChildPath $dependency.fileName
+
+  if (-Not ((Test-Path -Path $dependency.file -PathType Leaf) -And $dependency.hash -eq $(get-filehash $dependency.file).Hash)) {
+    # This downloads the file
+    Write-Host "Downloading $($dependency.url) ..."
+    try { 
+      $WebClient.DownloadFile($dependency.url, $dependency.file) 
+    } 
+    catch {
+      throw "Error downloading $($dependency.url)"
+    }
+    if (-not ($dependency.hash -eq $(get-filehash $dependency.file).Hash)) {
+      throw 'Hashes not match'
+    }
+  }
+}
 
 # Create Bootstrap script
 
@@ -34,7 +72,7 @@ $manifestFileName = Split-Path $Manifest -Leaf
 $bootstrapPs1Content = @"
 Set-PSDebug -Trace 1
 
-Add-AppxPackage -Path '$desktopAppInstaller' -DependencyPath '$vcLibs','$vcLibsUwp'
+Add-AppxPackage -Path '$($desktopAppInstaller.fileName)' -DependencyPath '$($vcLibs.fileName)','$($vcLibsUwp.fileName)'
 
 winget install -m '$manifestFileName'
 "@
