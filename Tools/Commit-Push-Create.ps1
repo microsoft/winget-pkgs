e77707571863f6cf28e600284879eae9ca791861
Bittu <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-08-03 16:03:52 +0530

    Update Commit-Push-Create.ps1

--- Tools/Commit-Push-Create.ps1
+++ Tools/Commit-Push-Create.ps1
@@ -6,34 +6,27 @@ if ($args[0] -and $args[1]) {
     $PkgId = Read-Host -Prompt 'Enter Package Name'
     $PkgVersion = Read-Host -Prompt 'Enter Package Version'
 }
-while ($keyInfo.Key -notin @('M', 'N', 'U','A','C','P')) {
-    Write-Host -NoNewLine "Commit Type: "
-    do {
-        $keyInfo = [Console]::ReadKey($false)
-    } until ($keyInfo.Key)
-}
-switch ($keyInfo.Key) {
-    'U' {$CommitType = "Update";Write-Host}
-    'N' {$CommitType = "New";Write-Host}
-    'M' {$CommitType = "Metadata";Write-Host}
-    'A' {$CommitType = "ARP";Write-Host}
-    'P' {$CommitType = "ProductCode";Write-Host}
-    'C' {Write-Host; $CommitType = Read-Host -Prompt 'Enter Custom Commit Message'}
-}
-if($keyInfo.Key -eq 'C') {
-    if ($CommitType -eq 'um') {
-        $CommitType = "Update Moniker"
-    } elseif ($CommitType -eq 'am') {
-        $CommitType = "Add Moniker"
-    } elseif ($CommitType -eq 'amd') {
-        $CommitType = "Add Metadata"
-    } elseif ($CommitType -eq 'umd') {
-        $CommitType = "Update Metadata"
-    } elseif ($CommitType -eq 'pi') {
-        $CommitType = "PackageIdentifier"
-    } elseif ($CommitType -eq 'mpc') {
-        $CommitType = "Moniker/ProductCode"
-    }
+$CommitType = Read-Host -Prompt 'Enter Commit Message'
+if ($CommitType -eq 'um') {
+    $CommitType = "Update Moniker"
+} elseif ($CommitType -eq 'am') {
+    $CommitType = "Add Moniker"
+} elseif ($CommitType -eq 'amd') {
+    $CommitType = "Add Metadata"
+} elseif ($CommitType -eq 'umd') {
+    $CommitType = "Update Metadata"
+} elseif ($CommitType -eq 'pi') {
+    $CommitType = "PackageIdentifier"
+} elseif ($CommitType -eq 'mpc') {
+    $CommitType = "Moniker/ProductCode"
+} elseif ($CommitType -eq 'u') {
+    $CommitType = "Update"
+} elseif ($CommitType -eq 'n') {
+    $CommitType = "New"
+} elseif ($CommitType -eq 'a') {
+    $CommitType = "ARP"
+} elseif ($CommitType -eq 'pc') {
+    $CommitType = "ProductCode"
 }
 git fetch upstream master
 git checkout -b "$PkgId-$PkgVersion" FETCH_HEAD
