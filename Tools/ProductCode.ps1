7f40600bb5e13efc6ee7d78a4f18ead02247cd90
Bittu <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-08-03 13:18:54 +0530

    Update ProductCode.ps1

--- Tools/ProductCode.ps1
+++ Tools/ProductCode.ps1
@@ -1,17 +1,18 @@
-﻿if($args[0]) {
-    $DownloadUrl = $args[0]
+﻿if($args) {
+    Foreach($DownloadUrl in $args) {
+        $WebClient = New-Object System.Net.WebClient
+        $Filename = [System.IO.Path]::GetFileName($DownloadUrl)
+        $location = "C:\Users\Bittu\Downloads\winget-pkgs\$FileName"
+        try {
+            $WebClient.DownloadFile($DownloadUrl, $location)
+        } catch {
+            Write-Host 'Error downloading file. Please run the script again.' -ForegroundColor Red
+            exit 1
+        }
+        $MyProductCode = Get-AppLockerFileInformation -Path $location | Select-Object -ExpandProperty Publisher | Select-Object BinaryName
+        Write-Host "Found ProductCode is: $($MyProductCode.BinaryName)"
+        Remove-Item -Path $location
+    }
 } else {
-    $DownloadUrl = Read-Host -Prompt "Enter Download Url"
+    Write-Host "No urls entered."
 }
-$WebClient = New-Object System.Net.WebClient
-$Filename = [System.IO.Path]::GetFileName($DownloadUrl)
-$location = "C:\Users\Bittu\Downloads\winget-pkgs\$FileName"
-try {
-    $WebClient.DownloadFile($DownloadUrl, $location)
-} catch {
-    Write-Host 'Error downloading file. Please run the script again.' -ForegroundColor Red
-    exit 1
-}
-$MyProductCode = Get-AppLockerFileInformation -Path $location | Select-Object -ExpandProperty Publisher | Select-Object BinaryName
-Write-Host "Found ProductCode is: $($MyProductCode.BinaryName)"
-Remove-Item -Path $location
