90a744611bf420152582ca458d0066ffb4d0e764
Bittu <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-08-05 22:59:25 +0530

    Update Commit-Push-Create.ps1

--- Tools/Commit-Push-Create.ps1
+++ Tools/Commit-Push-Create.ps1
@@ -1,40 +1,27 @@
 ﻿Write-Host "Commit Push and Create Pull Request"
 if ($args[0]) {$PkgId = $args[0]} else {$PkgId = Read-Host -Prompt 'Enter Package Name'}
 if ($args[1]) {$PkgVersion = $args[1]} else {$PkgVersion = Read-Host -Prompt 'Enter Package Version'}
-if ($args[2] -and $args[2] -in @('um','am','amd','umd','pi','mpc','u','n','a','pc','pcarp','r','url')) {
+if ($args[2] -and $args[2] -in @('um','am','amd','umd','pi','mpc','u','n','a','pc','pcarp','r','url','urlpc')) {
     $CommitType = $args[2]
 } else {
-    while ($CommitType -notin @('um','am','amd','umd','pi','mpc','u','n','a','pc','pcarp','r','url')) {
+    while ($CommitType -notin @('um','am','amd','umd','pi','mpc','u','n','a','pc','pcarp','r','url','urlpc')) {
         $CommitType = Read-Host -Prompt 'Enter Commit Message'
     }
 }
-if ($CommitType -eq 'um') {
-    $CommitType = "Update Moniker"
-} elseif ($CommitType -eq 'am') {
-    $CommitType = "Add Moniker"
-} elseif ($CommitType -eq 'amd') {
-    $CommitType = "Add Metadata"
-} elseif ($CommitType -eq 'umd') {
-    $CommitType = "Update Metadata"
-} elseif ($CommitType -eq 'pi') {
-    $CommitType = "PackageIdentifier"
-} elseif ($CommitType -eq 'mpc') {
-    $CommitType = "Moniker/ProductCode"
-} elseif ($CommitType -eq 'u') {
-    $CommitType = "Update"
-} elseif ($CommitType -eq 'n') {
-    $CommitType = "New"
-} elseif ($CommitType -eq 'a') {
-    $CommitType = "ARP"
-} elseif ($CommitType -eq 'pc') {
-    $CommitType = "ProductCode"
-} elseif ($CommitType -eq 'pcarp') {
-    $CommitType = "ProductCode/ARP"
-} elseif ($CommitType -eq 'r') {
-    $CommitType = "Remove"
-} elseif ($CommitType -eq 'url') {
-    $CommitType = "InstallerUrl"
-}
+if ($CommitType -eq 'um') {$CommitType = "Update Moniker"}
+elseif ($CommitType -eq 'am') {$CommitType = "Add Moniker"}
+elseif ($CommitType -eq 'amd') {$CommitType = "Add Metadata"}
+elseif ($CommitType -eq 'umd') {$CommitType = "Update Metadata"}
+elseif ($CommitType -eq 'pi') {$CommitType = "PackageIdentifier"}
+elseif ($CommitType -eq 'mpc') {$CommitType = "Moniker/ProductCode"}
+elseif ($CommitType -eq 'u') {$CommitType = "Update"}
+elseif ($CommitType -eq 'n') {$CommitType = "New"}
+elseif ($CommitType -eq 'a') {$CommitType = "ARP"}
+elseif ($CommitType -eq 'pc') {$CommitType = "ProductCode"}
+elseif ($CommitType -eq 'pcarp') {$CommitType = "ProductCode/ARP"}
+elseif ($CommitType -eq 'r') {$CommitType = "Remove"}
+elseif ($CommitType -eq 'url') {$CommitType = "InstallerUrl"}
+elseif ($CommitType -eq 'urlpc') {$CommitType = "ProductCode/InstallerUrl"}
 git fetch upstream master
 git checkout -b "$PkgId-$PkgVersion" FETCH_HEAD
 git add -A
