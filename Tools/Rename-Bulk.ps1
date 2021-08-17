06f6a028f660ea7b6b7085de5633756d71e9139f
Bittu <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-08-18 00:51:51 +0530

    Create Rename-Bulk.ps1

---
+++ Tools/Rename-Bulk.ps1
@@ -0,0 +1,11 @@
+﻿Foreach ($i in Get-ChildItem -File -Recurse | Select FullName) {
+    $content = Get-Content $i.FullName | Foreach {$_ -replace "PackageIdentifier: Joplin.Joplin","PackageIdentifier: Joplin.Joplin.Pre-release"} | Foreach {$_ -replace "Moniker: joplin","Moniker: joplin-prerelease"} | Foreach {$_ -replace "PackageName: Joplin","PackageName: Joplin (Pre-release)"}
+    Set-Content -Path $i.FullName -Value $content
+    if ($i.FullName.EndsWith('Joplin.Joplin.installer.yaml')) {
+        Rename-Item $i.FullName "Joplin.Joplin.Pre-release.installer.yaml"
+    } elseif ($i.FullName.EndsWith('Joplin.Joplin.locale.en-US.yaml')) {
+        Rename-Item $i.FullName "Joplin.Joplin.Pre-release.locale.en-US.yaml"
+    } elseif ($i.FullName.EndsWith('Joplin.Joplin.yaml')){
+        Rename-Item $i.FullName "Joplin.Joplin.Pre-release.yaml"
+    }
+}
