6dd5731f748a76e53223349a20cb0ff906fc0d20
Sander Cox <sander@resolume.com>
2023-11-07 01:05:01 +0100

    Environment variables ending with Path show error (#124995)
    When new environment variables ending with Path such as "VK_SDK_PATH"
    complain that it cannot execute the Get-Content on the empty path (as
    the variable was not set before). This shows errors when running the
    SandboxTest but those are not really errors.

--- Tools/SandboxTest.ps1
+++ Tools/SandboxTest.ps1
@@ -203,7 +203,7 @@ function Update-EnvironmentVariables {
   foreach($level in "Machine","User") {
     [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
         # For Path variables, append the new values, if they're not already in there
-        if($_.Name -match 'Path$') {
+        if($_.Name -match '^Path$') {
           $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
         }
         $_
