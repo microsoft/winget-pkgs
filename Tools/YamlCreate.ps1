b6f0b8c738127d11c88c4d596a3b0ebb7fa15d8d
Kaleb Luedtke <jluedtk@jci.com>
2024-01-02 09:54:39 -0600

    Bump Version

--- Tools/YamlCreate.ps1
+++ Tools/YamlCreate.ps1
@@ -166,7 +166,7 @@ if ($Settings) {
   exit
 }
 
-$ScriptHeader = '# Created with YamlCreate.ps1 v2.2.12'
+$ScriptHeader = '# Created with YamlCreate.ps1 v2.2.13'
 $ManifestVersion = '1.5.0'
 $PSDefaultParameterValues = @{ '*:Encoding' = 'UTF8' }
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
