ecc822122668cd87f1cafd15c87b39f1a00ae63d
Trenly <trenlymc@gmail.com>
2024-03-15 21:04:00 -0500

    Use Microsoft.WinGet.Client functions

--- Tools/Repology.ps1
+++ Tools/Repology.ps1
@@ -3,31 +3,17 @@
         [Parameter(Mandatory = $true, Position = 0)]
         [string] $PackageIdentifier,
         [Parameter(Mandatory = $true, Position = 1)]
-        [string] $RepologyProjectName
+        [string] $RepologyProjectName,
+        [switch] $IncludeLatest
     )
 
     $webPage = Invoke-WebRequest "https://repology.org/project/$RepologyProjectName/versions" -UseBasicParsing
-    $versions = $webPage.links.Where({ $_ -match 'cves\?' }).href | ForEach-Object { $_.split('=')[-1] }
+    $vulnerableVersions = $webPage.links.Where({ $_ -match 'cves\?' }).href | ForEach-Object { $_.split('=')[-1] }
 
-    $ManifestsFolder = Get-WinGetManifestPath $PackageIdentifier
-    $ManifestVersions = (Get-ChildItem $ManifestsFolder -Directory).Name
+    $ManifestVersions = (Get-WinGetPackage $PackageIdentifier).AvailableVersions
+    $versions =  $ManifestVersions.Where({$_ -in $vulnerableVersions })
 
-    return $versions.Where({$_ -in $ManifestVersions})
-}
-
-function Get-WinGetManifestPath {
-    Param(
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string] $PackageIdentifier
-    )
-    # Set the root folder where new manifests should be created
-    if (Test-Path -Path "$PSScriptRoot\..\manifests") {
-        $root = (Resolve-Path "$PSScriptRoot\..\manifests").Path
-    }
-    else {
-        $root = (Resolve-Path '.\').Path
-    }
-
-    $IdentifierParts = @($PackageIdentifier.Split('.'))
-    return  Join-Path $root $PackageIdentifier.ToLower()[0] @IdentifierParts
+    # The latest will be omitted by default to prevent accidental removal
+    if ($IncludeLatest) { return $versions}
+    return $versions.Where({$_ -ne $ManifestVersions[0]})
 }
