6aee9d0b72cd8c907d292b78d9a3b5de2ab99c86
Trenly <trenlymc@gmail.com>
2024-03-15 20:49:10 -0500

    Add tool for getting vulnerable repology versions

---
+++ Tools/Repology.ps1
@@ -0,0 +1,33 @@
+﻿function Get-RepologyVulnerableVersions {
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $PackageIdentifier,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string] $RepologyProjectName
+    )
+
+    $webPage = Invoke-WebRequest "https://repology.org/project/$RepologyProjectName/versions" -UseBasicParsing
+    $versions = $webPage.links.Where({ $_ -match 'cves\?' }).href | ForEach-Object { $_.split('=')[-1] }
+
+    $ManifestsFolder = Get-WinGetManifestPath $PackageIdentifier
+    $ManifestVersions = (Get-ChildItem $ManifestsFolder -Directory).Name
+
+    return $versions.Where({$_ -in $ManifestVersions})
+}
+
+function Get-WinGetManifestPath {
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $PackageIdentifier
+    )
+    # Set the root folder where new manifests should be created
+    if (Test-Path -Path "$PSScriptRoot\..\manifests") {
+        $root = (Resolve-Path "$PSScriptRoot\..\manifests").Path
+    }
+    else {
+        $root = (Resolve-Path '.\').Path
+    }
+
+    $IdentifierParts = @($PackageIdentifier.Split('.'))
+    return  Join-Path $root $PackageIdentifier.ToLower()[0] @IdentifierParts
+}
