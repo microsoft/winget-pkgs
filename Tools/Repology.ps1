d74f7e58d364342e9cf19f776d676638325ca73a
Kaleb Luedtke <jluedtk@jci.com>
2024-03-27 11:42:45 -0500

    Make a function to get all the vulnerabilities

--- Tools/Repology.ps1
+++ Tools/Repology.ps1
@@ -1,30 +1,4 @@
-﻿function Get-RepologyVulnerableVersions {
-    Param(
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string] $PackageIdentifier,
-        [Parameter(Mandatory = $true, Position = 1)]
-        [string] $RepologyProjectName,
-        [switch] $IncludeLatest
-    )
-
-    try {
-        $webPage = Invoke-WebRequest "https://repology.org/project/$RepologyProjectName/versions" -UseBasicParsing
-        $vulnerableVersions = $webPage.links.Where({ $_ -match 'cves\?' }).href | ForEach-Object { $_.split('=')[-1] }
-
-        $ManifestVersions = (Find-WinGetPackage $PackageIdentifier).AvailableVersions
-    } catch {
-        Out-Null
-    }
-    if (!$vulnerableVersions -or !$ManifestVersions) { return $null }
-
-    $versions = $ManifestVersions.Where({ $_ -in $vulnerableVersions })
-
-    # The latest will be omitted by default to prevent accidental removal
-    if ($IncludeLatest) { return $versions }
-    return $versions.Where({ $_ -ne $ManifestVersions[0] })
-}
-
-function Get-RepologyWingetIds {
+﻿function Get-RepologyWingetIds {
     Param(
         [Parameter(Mandatory = $true, Position = 0)]
         [string] $RepologyProjectName
@@ -41,4 +15,51 @@ function Get-RepologyWingetIds {
 
     $wingetManifestPaths = $wingetManifestPaths -replace 'https://github.com/microsoft/winget-pkgs/tree/master/manifests/', ''
     return $wingetManifestPaths.ForEach({ ($_.Split('/') | Select-Object -Skip 1 | Select-Object -SkipLast 1) -join '.' }) | Select-Object -Unique
+}
+
+function Get-RepologyVulnerableVersions {
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string] $RepologyProjectName,
+        [switch] $IncludeLatest
+    )
+
+    try {
+        $webPage = Invoke-WebRequest "https://repology.org/project/$RepologyProjectName/versions" -UseBasicParsing
+        $vulnerableVersions = $webPage.links.Where({ $_ -match 'cves\?' }).href | ForEach-Object { $_.split('=')[-1] }
+        $wingetManifestPaths = $webPage.links.Where({ $_.href -match 'winget-pkgs' }).href | Select-Object -Unique
+    } catch {
+        Out-Null
+    }
+    if (!$vulnerableVersions -or !$wingetManifestPaths) { return $null }
+    $wingetManifestPaths = $wingetManifestPaths -replace 'https://github.com/microsoft/winget-pkgs/tree/master/manifests/', ''
+    $wingetPackageIdentifiers = $wingetManifestPaths.ForEach({ ($_.Split('/') | Select-Object -Skip 1 | Select-Object -SkipLast 1) -join '.' }) | Select-Object -Unique
+    $vulnerablePackages = @()
+    $wingetPackageIdentifiers | ForEach-Object {
+        $ManifestVersions = (Find-WinGetPackage $_).AvailableVersions
+        if (!$ManifestVersions) { continue }
+        $versions = $ManifestVersions.Where({ $_ -in $vulnerableVersions })
+        $vulnerablePackages += New-Object psobject -Property @{
+            RepologyProjectName = $RepologyProjectName
+            PackageIdentifier = $_
+            Versions = $(
+                if ($IncludeLatest) {
+                    $versions.Where({ $_ -ne $ManifestVersions[0] })
+                } else {
+                    $versions
+                }
+            )
+        }
+    }
+    return $vulnerablePackages.where({$_.Versions.count -gt 0})
+}
+
+function Get-AllRepologyVulnerableVersions {
+    $webPage = Invoke-WebRequest "https://repology.org/projects/?inrepo=winget&vulnerable=1" -UseBasicParsing
+    $repologyProjectLinks = $webPage.links.href.Where({$_ -match '/versions$'})
+    $repologyProjectIds = $repologyProjectLinks.Replace('/project/','').Replace('/versions','')
+
+    $vulnerablePackages = @()
+    $repologyProjectIds | ForEach-Object { $vulnerablePackages += Get-RepologyVulnerableVersions -RepologyProjectName $_}
+    return $vulnerablePackages
 }
