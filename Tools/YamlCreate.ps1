384d12d7a2b5c11a0098e2e3cb9f81574747c55e
Kaleb Luedtke <trenlymc@gmail.com>
2025-12-22 17:36:41 -0600

    Add Font Support

--- Tools/YamlCreate.ps1
+++ Tools/YamlCreate.ps1
@@ -236,8 +236,8 @@ if ($Settings) {
   exit
 }
 
-$ScriptHeader = '# Created with YamlCreate.ps1 v2.5.0'
-$ManifestVersion = 'latest'
+$ScriptHeader = '# Created with YamlCreate.ps1 v2.6.0'
+$ManifestVersion = '1.10.0'
 $PSDefaultParameterValues = @{ '*:Encoding' = 'UTF8' }
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 $ofs = ', '
@@ -273,11 +273,18 @@ $useDirectSchemaLink = if ($env:GITHUB_ACTIONS -eq $true) {
 } else {
   (Invoke-WebRequest "https://aka.ms/winget-manifest.version.$ManifestVersion.schema.json" -UseBasicParsing).Content -match '<!doctype html>'
 }
+
+if ($ManifestVersion -ne 'latest') {
+  $FullManifestVersion = "v$ManifestVersion"
+} else {
+  $FullManifestVersion = $ManifestVersion
+}
+
 $SchemaUrls = @{
-  version       = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$ManifestVersion/manifest.version.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.version.$ManifestVersion.schema.json" }
-  defaultLocale = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$ManifestVersion/manifest.defaultLocale.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.defaultLocale.$ManifestVersion.schema.json" }
-  locale        = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$ManifestVersion/manifest.locale.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.locale.$ManifestVersion.schema.json" }
-  installer     = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$ManifestVersion/manifest.installer.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.installer.$ManifestVersion.schema.json" }
+  version       = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$FullManifestVersion/manifest.version.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.version.$ManifestVersion.schema.json" }
+  defaultLocale = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$FullManifestVersion/manifest.defaultLocale.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.defaultLocale.$ManifestVersion.schema.json" }
+  locale        = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$FullManifestVersion/manifest.locale.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.locale.$ManifestVersion.schema.json" }
+  installer     = if ($useDirectSchemaLink) { "https://raw.githubusercontent.com/microsoft/winget-cli/master/schemas/JSON/manifests/$FullManifestVersion/manifest.installer.$ManifestVersion.json" } else { "https://aka.ms/winget-manifest.installer.$ManifestVersion.schema.json" }
 }
 
 # Fetch Schema data from github for entry validation, key ordering, and automatic commenting
@@ -292,6 +299,13 @@ try {
   $InstallerEntryProperties = (ConvertTo-Yaml $InstallerSchema.definitions.Installer.properties | ConvertFrom-Yaml -Ordered).Keys
   $InstallerDependencyProperties = (ConvertTo-Yaml $InstallerSchema.definitions.Dependencies.properties | ConvertFrom-Yaml -Ordered).Keys
   $AppsAndFeaturesEntryProperties = (ConvertTo-Yaml $InstallerSchema.definitions.AppsAndFeaturesEntry.properties | ConvertFrom-Yaml -Ordered).Keys
+
+  # Update the manifest version in case `latest` was specified
+  $ManifestVersion = $VersionSchema.properties.ManifestVersion.default
+  # Update the schema URLs to reflect the correct version for use in the manifest header
+  @($SchemaUrls.Keys) | ForEach-Object {
+    $SchemaUrls[$_] = "https://aka.ms/winget-manifest.$_.$ManifestVersion.schema.json"
+  }
 } catch {
   # Here we want to pass the exception as an inner exception for debugging if necessary
   throw [System.Net.WebException]::new('Manifest schemas could not be downloaded. Try running the script again', $_.Exception)
@@ -851,6 +865,22 @@ Function Read-NestedInstaller {
           }
         }
       }
+
+      if ($_EffectiveType -eq 'font') {
+        # Prompt to see if multiple entries are needed
+        $_menu = @{
+          entries       = @(
+            '[Y] Yes'
+            '*[N] No'
+          )
+          Prompt        = 'Do you want to create another font entry?'
+          DefaultString = 'N'
+        }
+        switch ( Invoke-KeypressMenu -Prompt $_menu['Prompt'] -Entries $_menu['Entries'] -DefaultString $_menu['DefaultString']) {
+          'Y' { $AnotherNestedInstaller = $true }
+          default { $AnotherNestedInstaller = $false }
+        }
+      }
       $_NestedInstallerFiles += $_InstallerFile
     } until (!$AnotherNestedInstaller)
     $_Installer['NestedInstallerFiles'] = $_NestedInstallerFiles
@@ -1306,6 +1336,11 @@ Function Read-InstallerEntry {
   # If there are additional entries, run this function again to fetch the values and add them to the installers array
   if ($AnotherInstaller -eq '0') {
     Write-Host; Read-InstallerEntry
+  }
+
+  # If the app folder is in manifests and the installer type is font, change the app folder to point at fonts
+  if ($script:AppFolder -match 'manifests' -and (Get-EffectiveInstallerType $_Installer) -eq 'font') {
+    $script:AppFolder = $script:AppFolder -replace 'manifests', 'fonts'
   }
 }
 
@@ -2704,14 +2739,39 @@ if ($ScriptSettings.ContinueWithExistingPRs -ne 'always' -and $script:Option -ne
 $script:AppFolder = Join-Path $ManifestsFolder -ChildPath $PackageIdentifier.ToLower().Chars(0) | Join-Path -ChildPath $PackageIdentifierFolder | Join-Path -ChildPath $PackageVersion
 $script:FontFolder = Join-Path $FontsFolder -ChildPath $PackageIdentifier.ToLower().Chars(0) | Join-Path -ChildPath $PackageIdentifierFolder | Join-Path -ChildPath $PackageVersion
 
+# Attempt to see if the old package exists in the manifests folder, font folder, or both
+$script:DestinationFolder = $null;
+if (Test-Path -Path (Split-Path $script:AppFolder)) {
+  $script:DestinationFolder = $script:AppFolder
+}
+if (Test-Path -Path (Split-Path $script:FontFolder)) {
+  if ($script:DestinationFolder) {
+    $script:DestinationFolder = @($script:DestinationFolder; $script:FontFolder)
+  } else {
+    $script:DestinationFolder = $script:FontFolder
+  }
+}
+if ($script:DestinationFolder -and $script:DestinationFolder.Count -gt 1) {
+  $_menu = @{
+    entries       = @('[1] Manifests Folder'; '[2] Fonts Folder')
+    Prompt        = 'The package exists in both the manifests and fonts folder. Which folder do you want to use?'
+    DefaultString = '1'
+  }
+  switch ( Invoke-KeypressMenu -Prompt $_menu['Prompt'] -Entries $_menu['Entries'] -DefaultString $_menu['DefaultString'] ) {
+    '1' { $script:AppFolder = $script:AppFolder }
+    '2' { $script:AppFolder = $script:FontFolder }
+  }
+} elseif ($script:DestinationFolder -and $script:DestinationFolder.Count -eq 1) {
+  $script:AppFolder = $script:DestinationFolder
+}
+
 # If the user selected `NewLocale` or `EditMetadata` the version *MUST* already exist in the folder structure
 if ($script:Option -in @('NewLocale'; 'EditMetadata'; 'RemoveManifest')) {
   # Try getting the old manifests from the specified folder
   if (Test-Path -Path "$AppFolder\..\$PackageVersion") {
     $script:OldManifests = Get-ChildItem -Path "$AppFolder\..\$PackageVersion"
     $LastVersion = $PackageVersion
-  }
-  elseif (Test-Path -Path "$FontFolder\..\$PackageVersion") {
+  } elseif (Test-Path -Path "$FontFolder\..\$PackageVersion") {
     $script:OldManifests = Get-ChildItem -Path "$FontFolder\..\$PackageVersion"
     $LastVersion = $PackageVersion
     # Intentionally override AppFolder here to ensure the rest of the script works as expected
@@ -2728,11 +2788,10 @@ if ($script:Option -in @('NewLocale'; 'EditMetadata'; 'RemoveManifest')) {
     if (Test-Path -Path "$AppFolder\..\$PromptVersion") {
       $script:OldManifests = Get-ChildItem -Path "$AppFolder\..\$PromptVersion"
       $script:AppFolder = Join-Path (Split-Path $AppFolder) -ChildPath $LastVersion
-    }
-    elseif (Test-Path -Path "$FontsFolder\..\$PromptVersion") {
-      $script:OldManifests = Get-ChildItem -Path "$FontsFolder\..\$PromptVersion"
+    } elseif (Test-Path -Path "$FontFolder\..\$PromptVersion") {
+      $script:OldManifests = Get-ChildItem -Path "$FontFolder\..\$PromptVersion"
       # Intentionally use AppFolder here to ensure the rest of the script works as expected
-      $script:AppFolder = Join-Path (Split-Path $FontsFolder) -ChildPath $LastVersion
+      $script:AppFolder = Join-Path (Split-Path $FontFolder) -ChildPath $LastVersion
     }
     # If a new version is entered, we need to be sure to update the folder for writing manifests
     $LastVersion = $PromptVersion
@@ -2742,7 +2801,7 @@ if ($script:Option -in @('NewLocale'; 'EditMetadata'; 'RemoveManifest')) {
 
 # If the user selected `QuickUpdateVersion`, the old manifests must exist
 # If the user selected `New`, the old manifest type is specified as none
-if (-not (Test-Path -Path "$AppFolder\..") -and -not (Test-Path -Path "$FontsFolder\..")) {
+if (-not (Test-Path -Path "$AppFolder\..") -and -not (Test-Path -Path "$FontFolder\..")) {
   if ($script:Option -in @('QuickUpdateVersion', 'Auto')) {
     Write-Host -ForegroundColor Red 'This option requires manifest of previous version of the package. If you want to create a new package, please select Option 1.'
     Invoke-CleanExit
